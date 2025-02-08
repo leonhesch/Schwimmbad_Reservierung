e3a89926d22865ee546e2818f90b89119beb0c98
Leon Hesch <lhesch97@gmail.com>
2025-02-08 14:27:34 +0100

    Einbindung von MaterialDesign für GUI

--- Implementierung/Schwimmbad_Wpf/Gruppenreservierungen/MainWindow.xaml.cs
+++ Implementierung/Schwimmbad_Wpf/Gruppenreservierungen/MainWindow.xaml.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Gruppenreservierungen { }
+
+
