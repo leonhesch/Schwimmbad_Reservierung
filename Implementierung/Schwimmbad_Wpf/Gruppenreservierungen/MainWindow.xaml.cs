bae9ac68e0ec735d218712914470fe405be163fd
Leon Hesch <lhesch97@gmail.com>
2025-02-12 17:41:51 +0100

    Platzhalter Methoden

--- Implementierung/Schwimmbad_Wpf/Gruppenreservierungen/MainWindow.xaml.cs
+++ Implementierung/Schwimmbad_Wpf/Gruppenreservierungen/MainWindow.xaml.cs
@@ -5,6 +5,44 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace Gruppenreservierungen { }
+namespace Gruppenreservierungen
+{
+    public class Reservation
+    {
+        public string GroupName { get; set; }
+        public int GroupSize { get; set; }
+        public string Date { get; set; }
+        public string Requirements { get; set; }
+    }
 
+    public partial class MainWindow : Window
+    {
+        private const string FilePath = "reservierungen.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BtnSaveReservation_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        
+        private void BtnRefreshReservations_Click(object sender, RoutedEventArgs e)
+        {
+         
+           
+        }
+
+    
+        private void BtnSearchReservation_Click(object sender, RoutedEventArgs e)
+        {
+         
+            
+        }
+
+    }
+    }
 
